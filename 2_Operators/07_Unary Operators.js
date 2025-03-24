aad7b3b57633be43f6b05773f832ca803d700df1
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-24 21:49:22 +0530

    Unary Operators

---
+++ 2_Operators/07_Unary Operators.js
@@ -0,0 +1,11 @@
+console.log(+ "10");   // 10
+console.log(- true);   // -1
+let x = 5;
+console.log(++x);      // 6
+console.log(x--);      // 6, then x becomes 5
+console.log(!0);       // true
+console.log(typeof "Hello"); // "string"
+console.log(~5);       // -6
+let obj = { a: 1, b: 2 };
+delete obj.a;
+console.log(obj);      // { b: 2 }
