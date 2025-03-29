81ac6d673bf086b6a1c5060d7924e5ae4263f8ef
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-29 21:42:39 +0530

    Break

---
+++ 3_Conditional Statements/06-Break Statement.js
@@ -0,0 +1,12 @@
+const fruit = "Mango";
+
+switch (fruit) {
+    case "Apple":
+        console.log("Apple is healthy.");
+        break;
+    case "Mango":
+        console.log("Mango is delicious.");
+        break;
+    default:
+        console.log("No fruit chosen.");
+}
